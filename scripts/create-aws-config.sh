10fd92fa9ca42b4342ea0e12fc5d135a068d58ee
Johannes Konings <johannes.konings@outlook.com>
2024-12-28 19:17:23 +0100

    update: use aws-sso-util

--- scripts/create-aws-config.sh
+++ scripts/create-aws-config.sh
@@ -10,20 +10,12 @@ if [ -z "$SSO_START_URL" ] ; then
     echo "ERROR: SSO_START_URL not set in .env file!"
     exit 1
 fi
-if [ -z "$SSO_SESSION_NAME" ] ; then
-    echo "ERROR: SSO_SESSION_NAME not set in .env file!"
-    exit 1
-fi
 if [ -z "$SSO_REGION" ] ; then
     echo "ERROR: REGION not set in .env file!"
     exit 1
 fi
 
-mkdir -p ~/.aws
-touch ~/.aws/config
-cat << EOF > ~/.aws/config
-[sso-session $SSO_SESSION_NAME]
-sso_start_url = $SSO_START_URL
-sso_region = $SSO_REGION
-sso_registration_scopes = sso:account:access
-EOF
+aws-sso-util configure populate \
+--sso-start-url $SSO_START_URL \
+--sso-region $SSO_REGION \
+--region $SSO_REGION
--- scripts/create-steampipe-aws-config.sh
+++ scripts/create-steampipe-aws-config.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 cat << EoF > ~/.steampipe/config/aws.spc
-connection "aws" {
-  plugin = "aws"
-}
-
 connection "aws_all" {
   plugin      = "aws" 
   type        = "aggregator"
@@ -18,8 +14,8 @@ ALLOWED_ROLES="AWSReadOnlyAccess,AWSAdministratorAccess"
 cat ~/.aws/config | grep -E '^\[profile' | sed 's/\[profile //g' | sed 's/\]//g' | while read profile_name ; do
 
     # check if suffix of profile match with one of the list allowed_roles
-    suffix=$(echo $profile_name | sed 's/.*-//g')
-    hit=$(echo $ALLOWED_ROLES | grep $suffix || echo "")
+    suffix=$(echo $profile_name | sed 's/.*\.//g' | sed 's/.*-//g')
+    hit=$(echo $ALLOWED_ROLES | grep -w $suffix || echo "")
     if [ -z "$hit" ] ; then
         echo "Skipping profile $profile_name, suffix $suffix not in allowed roles list" >&2
         continue
